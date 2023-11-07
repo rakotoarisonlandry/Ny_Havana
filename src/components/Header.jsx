e775e97b41d69432c84ac9b5ebdc7b9135c26c5a
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-07 08:53:04 +0100

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import logo from "../img/logo.png";
 import LogoHome from "../img/carbon_home.svg";
+import { Link } from "react-router-dom";
 function Header() {
   return (
-    <div className="flex  text-[#fff] pl-9 space-x-[18px]">
-      <div className="flex space-x-3 pt-3 items-center">
-        <img src={logo} alt="NyHavana" className="w-9" />
-        <h2>Ny Havana</h2>
+    <div className="flex  text-[#fff] pl-7 space-x-2">
+      <div className="flex space-x-3 pt-3  items-center">
+        <img src={logo} alt="NyHavana" className="w-11" />
+        <h2 className="text-2xl">Ny Havana</h2>
       </div>
-      <ul className=" flex items-center text-center text-[16px] space-x-11 pt-2 pl-[900px]">
-        <li>
-          <img src={LogoHome} alt="NyHavana" className="w-[24.5] h-[23.5]" />
-        </li>
-        <li>Blog</li>
-        <li>Contact Us</li>
+      <ul className=" flex items-center text-center text-[17px] space-x-16 pt-2 pl-[780px]">
+        <Link to="/" className="pt-6">
+          <li className="font-bold text-[#80a072] pb-3">Home</li>
+          <div className="w-2 h-2 bg-[#80a072] ml-5  rounded-full relative"></div>
+        </Link>
+        <Link>Blog</Link>
+        <Link>Contact Us</Link>
       </ul>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../index.css"
-import Header from "../components/Header";
+import "../index.css";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
 function Home() {
   useEffect(() => {
     axios
@@ -14,8 +17,17 @@ function Home() {
 
   return (
     <>
-      <div>
-        Home
+      <div className="w-full pt-32 pl-7 flex">
+        <div className="w-[50%]">
+          <Typography variant="h2" className="text-[90px]">
+            Compagnie Malgache D’assurance et de Reassurance.
+          </Typography>
+          <Typography variant="body1" className="text-base">
+            “...Bien Assuré, lendemain sécurisé.“
+          </Typography>
+          <div></div>
+        </div>
+        <div></div>
       </div>
     </>
   );
