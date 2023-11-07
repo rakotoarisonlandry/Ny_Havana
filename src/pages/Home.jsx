8e1458ac604e2301ded9d3fd29105a09934e1eec
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-07 13:16:40 +0100

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,21 +17,21 @@ function Home() {
 
   const customStyle = {
     fontWeight: 700, // La valeur 800 correspond à "extrabold" en termes de poids de police
-    fontSize: "47px", // Taille de police personnalisée
+    fontSize: "53px", // Taille de police personnalisée
   };
   return (
     <>
-      <div className="w-full pt-32 pl-7 flex">
+      <div className="w-full mt-32 ml-12 flex ">
         <div className="w-[50%] space-y-7 ">
-          <Typography variant="h2" className="text-gradient" style={customStyle}>
+          <Typography variant="h2" className="text-gradient " style={customStyle}>
             Compagnie Malgache D’assurance et de Reassurance.
           </Typography>
           <Typography variant="body1" className="text-base text-[#AE9F9F]">
             “...Bien assuré,lendemain sécurisé.“
           </Typography>
           <div className="flex space-x-4">
-            <button className="p-4 rounded-md text-[#fff]  font-bold bg-gradient-to-r from-[#D4145A] to-[#FBB03B]">Votre economie ici</button>
-            <button className="text-[#fff] rounded-md border-[1px] border-[#FBB03B] p-4">Contact Us</button>
+            <button className="pt-1 pb-3 pl-7 pr-7 rounded-md text-[#fff]  font-bold bg-gradient-to-r from-[#D4145A] to-[#FBB03B]">Votre economie ici</button>
+            <button className="text-[#fff] rounded-md border-[1px] border-[#FBB03B] pt-3 pb-3 pl-7 pr-7">Contact Us</button>
           </div>
         </div>
         <div></div>
