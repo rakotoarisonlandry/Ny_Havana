71f6be34615d1b243676b9a6a8aefd90d9be2255
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-14 13:01:25 +0100

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="w-full flex top-1 select-none text-[#fff] pl-7 space-x-2">
-      <Link to="/" className="flex space-x-3 pt-3 pb-3  items-center">
+      <Link to="/" className="w-16 flex space-x-3 pt-3 pb-3  items-center">
         <img src={logo} alt="NyHavana" className="w-8" />
         <h2 className="text-2xl font-bold">Ny Havana</h2>
       </Link>
@@ -20,7 +20,7 @@ function Header() {
           to="/login"
           className="text-[#FBB03B] font-semibold select-none rounded-xl border-[1px] border-[#FBB03B] pt-2 pb-2 pl-9 pr-9"
         >
-          login
+          Login
         </Link>
       </ul>
     </div>
