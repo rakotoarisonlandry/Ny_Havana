285892fa3eea6689fa48098264f319a1f95765c9
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-14 06:57:56 +0100

    new commit

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -4,12 +4,18 @@ import "react-quill/dist/quill.snow.css";
 
 function Write() {
   const [value, setValue] = useState("");
+  console.log(value)
   return (
     <div className="add">
       <div className="content">
         <input type="text" placeholder="Title" />
         <div className="editorContainer">
-          <ReactQuill  className="editor" theme="snow" value={value} onChange={setValue} />
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
         </div>
       </div>
       <div className="menu">
