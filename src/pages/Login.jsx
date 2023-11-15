d753105a9193db71dc1f2cfba8ea9fc44173ec9d
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-15 13:54:20 +0100

    last commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,17 +4,8 @@ import { Link } from "react-router-dom";
 function Login() {
   return (
     <section className="bg-gray-900">
-      <div>
-        <img src="" alt="" />
-      </div>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
-          <img className="w-8 h-8 mr-2" src="" alt="logo" />
-          Flowbite
-        </a>
+        
         <div className="w-full  rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
@@ -88,12 +79,12 @@ function Login() {
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
-                <a
+                <Link
                   href="#"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Sign up
-                </a>
+                </Link>
               </p>
             </form>
           </div>
