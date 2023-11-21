905c32caba67126d1ab75b2d5499bca606fe11ce
rakotorisonlandry <rakotorisonlandry@gmail.com>
2023-11-21 08:05:07 +0100

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,36 @@ import { Link } from "react-router-dom";
 import { AuthContexte } from "../context/AuthContext";
 
 function Header() {
-  const { currentUser } = useContext(AuthContexte);
+  const { currentUser, logout } = useContext(AuthContexte);
   return (
     <div className="w-full flex top-1 select-none text-[#fff] pl-7 space-x-2">
       <Link to="/" className="w-16 flex space-x-3 pt-3 pb-3  items-center">
         <img src={logo} alt="NyHavana" className="w-8" />
         <h2 className="text-2xl font-bold">Ny Havana</h2>
       </Link>
-      <ul className="flex items-center text-center text-[17px] space-x-16 pt-2 pl-[650px]">
+      <ul className="flex items-center text-center text-[17px] space-x-16 pt-2 pl-[780px]">
         <Link to="/" className="pt-6">
           <li className="font-bold text-[#80a072] pb-3">Accueil</li>
           <div className="w-2 h-2 bg-[#80a072] ml-5  rounded-full relative"></div>
         </Link>
         <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contacts</Link>
-        <Link
+        {/* <Link
           to="/login"
           className="text-[#FBB03B] font-semibold select-none rounded-xl border-[1px] border-[#FBB03B] pt-2 pb-2 pl-9 pr-9"
         >
           Login
-        </Link>
+        </Link> */}
         <span>{currentUser?.username}</span>
+        {currentUser ? (
+          <span onClick={logout}>Logout</span>
+        ) : (
+          <Link
+            to="/login"
+            className="text-[#FBB03B] font-semibold select-none rounded-xl border-[1px] border-[#FBB03B] pt-2 pb-2 pl-9 pr-9"
+          >
+            Login
+          </Link>
+        )}
       </ul>
     </div>
   );
